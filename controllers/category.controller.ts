aa4a5069ca84dda229c08706d6d9c85c8b9aaec1
iv4nperez <iv4n.perez_@hotmail.com>
2023-08-19 12:49:14 -0600

    ajuste

--- controllers/category.controller.ts
+++ controllers/category.controller.ts
@@ -7,15 +7,13 @@ export const getCategories = async (req: Request, res: Response) => {
     try {
         const { lang = "es" } = req.query;
         const categories = await categoryModel.find({ isDeleted: false }).sort({ name: 1 });
-        res.json({
-            data: categories.map((category: any) => {
-                return {
-                    id: category.id,
-                    name: category.name[String(lang)],
-                    imageUrl: category.imageUrl
-                }
-            })
-        })
+        res.json(categories.map((category: any) => {
+            return {
+                id: category.id,
+                name: category.name[String(lang)],
+                imageUrl: category.imageUrl
+            }
+        }))
     } catch (error) {
         console.log(error)
         res.status(500).json({
